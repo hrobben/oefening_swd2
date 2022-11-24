cc5dc7c9b4cb2876e7df466fbf0e7bc65243c138
sys72740hro <Hcbdag@635>
2022-11-24 13:08:21 +0100

    update

--- index.php
+++ index.php
@@ -39,9 +39,10 @@ try {
     echo $v;
     if ($k=='id') {
       $id = strip_tags($v);
+      $l= 'delete.php?id='.$id;
     }
     if ($k=='country') {
-      echo '<td><a href=\'delete.php?id=\'' .$id.'>X</a></td>';
+      echo "<td><a href='$l'>X</a></td>";
     }
   }
 } catch(PDOException $e) {
