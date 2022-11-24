e1eb8ce7d37e8a1510fc37ed6f0daf5b09b23d8f
sys72740hro <Hcbdag@635>
2022-11-24 13:16:21 +0100

    update

--- delete.php
+++ delete.php
@@ -1,3 +1,24 @@
 <?php
 
-    echo '<h1>Verwijderen van id '.$_GET['id'].'<h1>';
+   echo '<h1>Verwijderen van id '.$_GET['id'].'<h1>';
+
+   if ($_GET['id']) {
+       include_once ('openDB.php');
+       $id = $_GET['id'];
+       try {
+           // set the PDO error mode to exception
+           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+           // sql to delete a record
+           $sql = "DELETE FROM myTable WHERE id=$id";
+
+           // use exec() because no results are returned
+           $conn->exec($sql);
+           echo "Record deleted successfully<br>";
+           echo "<a href='index.php'>terug naar index</a>";
+       } catch (PDOException $e) {
+           echo $sql . "<br>" . $e->getMessage();
+        }
+
+       $conn = null;
+   }
