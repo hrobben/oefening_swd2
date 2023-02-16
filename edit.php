ebadfa097c58b70a96d64f942e3e4a771c2c7324
sys72740hro <Hcbdag@635>
2023-02-16 11:57:01 +0100

    image upload toegepast

--- edit.php
+++ edit.php
@@ -26,13 +26,22 @@ if ($_GET['id']) {
                 }
             }
             // form
-            echo "<form action=\"edit.php\" method='post'>
-                  <input type='hidden' name='id' value=$id>
+            echo "
+                    <form action=\"edit.php\" method='post'>
+                    <input type='hidden' name='id' value=$id>
                  ";
             foreach ($table_fields as $row) {
                 if ($row <> 'id') {
-                    echo "<label for=\"$row\">$row:</label><br>
+                    if ($row == 'image') {
+                        echo '
+                          Select image to upload:
+                              <input type="file" name="fileToUpload" id="fileToUpload">
+                        ';
+                    } else {
+                        echo "<label for=\"$row\">$row:</label><br>
                           <input type=\"text\" id=\"$row\" name=\"$row\" value='" . $$row . "'><br>";
+                    }
+
                 }
             }
             // submit
@@ -50,9 +59,61 @@ if ($_GET['id']) {
 }
 
 if ($_POST) {
+
     try {
         foreach ($table_fields as $row) {
             $$row = $_POST[$row];
+            if ($row == 'image') {
+                $image = $_POST['fileToUpload'];
+                    $target_dir = "uploads/";
+                    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+                    $uploadOk = 1;
+                    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+                    // Check if image file is a actual image or fake image
+                    if(isset($_POST["fileToUpload"])) {
+                        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+                        if($check !== false) {
+                            echo "File is an image - " . $check["mime"] . ".";
+                            $uploadOk = 1;
+                        } else {
+                            echo "File is not an image.";
+                            $uploadOk = 0;
+                        }
+                    }
+
+                    // Check if file already exists
+                    if (file_exists($target_file)) {
+                        echo "Sorry, file already exists.";
+                        $uploadOk = 0;
+                    }
+
+                    // Check file size
+                    if ($_FILES["fileToUpload"]["size"] > 500000) {
+                        echo "Sorry, your file is too large.";
+                        $uploadOk = 0;
+                    }
+
+                    // Allow certain file formats
+                    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                        && $imageFileType != "gif" ) {
+                        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                        $uploadOk = 0;
+                    }
+
+                    // Check if $uploadOk is set to 0 by an error
+                    if ($uploadOk == 0) {
+                        echo "Sorry, your file was not uploaded.";
+                        // if everything is ok, try to upload file
+                    } else {
+                        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                            echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+                        } else {
+                            echo "Sorry, there was an error uploading your file.";
+                        }
+                    }
+
+            }
         }
 
         $sql = "UPDATE $tbl SET ";
